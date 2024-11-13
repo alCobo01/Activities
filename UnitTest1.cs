48e102720c5c1aa1fb227cb50a5dc9c1418b5557
Alvaro Cobo <alvaro.cobo.7e6@itb.cat>
2024-11-13 11:34:37 +0100

    Exercici 18 fet. Closes #1

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -6,9 +6,75 @@ namespace TestActivities
     public class UnitTest1
     {
         [TestMethod]
-        public void ValidNum()
+        public void RangeLimitInsideOne()
         {
-            
+            //Arrange
+            int num = 0;
+            bool test;
+            //Act
+            test = Utils.IsValidAge(num);
+            //Assert
+            Assert.IsTrue(test);
+        }
+
+        [TestMethod]
+        public void RangeLimitInsideTwo()
+        {
+            //Arrange
+            int num = 120;
+            bool test;
+            //Act
+            test = Utils.IsValidAge(num);
+            //Assert
+            Assert.IsTrue(test);
+        }
+
+        [TestMethod]
+        public void RangeLimitOutsideOne()
+        {
+            //Arrange
+            int num = 121;
+            bool test;
+            //Act
+            test = Utils.IsValidAge(num);
+            //Assert
+            Assert.IsFalse(test);
+        }
+
+        [TestMethod]
+        public void RangeLimitOutsideTwo()
+        {
+            //Arrange
+            int num = -1;
+            bool test;
+            //Act
+            test = Utils.IsValidAge(num);
+            //Assert
+            Assert.IsFalse(test);
+        }
+
+        [TestMethod]
+        public void OutsideOfRangeOne()
+        {
+            //Arrange
+            int num = 132;
+            bool test;
+            //Act
+            test = Utils.IsValidAge(num);
+            //Assert
+            Assert.IsFalse(test);
+        }
+
+        [TestMethod]
+        public void OutsideOfRangeTwo()
+        {
+            //Arrange
+            int num = -32;
+            bool test;
+            //Act
+            test = Utils.IsValidAge(num);
+            //Assert
+            Assert.IsFalse(test);
         }
     }
 }
