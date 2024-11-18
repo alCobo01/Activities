f74ffc32791489a41b6b8433cd746beafcf71a42
Alvaro Cobo <alvaro.cobo.7e6@itb.cat>
2024-11-18 11:56:05 +0100

    Exercici 12 fet, closes #8

--- Activities/Program.cs
+++ Activities/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace ActivitatsPrimerTema
+{
+    public class Program
+    {
+        public static double CelsiusToFarenheit(double temperature)
+        {
+            return (1.8 * temperature) + 32;
+        }
+
+        public static void Main()
+        {
+            const string PrimerMissatge = "Introdueix la temperatura en Celsius (Cº): ";
+            const string MissatgeResultat = "La teva temperatura en Farenheit és";
+            const string MissatgeError = "Format incorrecte";
+
+            double numUser;
+
+            Console.Write(PrimerMissatge);
+            try
+            {
+                numUser = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine($"{MissatgeResultat} {CelsiusToFarenheit(numUser)}");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine(MissatgeError);
+            }
+        }
+    }
+}
--- T2-Activitats.md
+++ T2-Activitats.md
@@ -293,6 +293,38 @@ namespace ActivitatsPrimerTema
 ## Exercici 12
 ```c#
 //PRE:
+using System;
+
+namespace ActivitatsPrimerTema
+{
+    public class Program
+    {
+        public static double CelsiusToFarenheit(double temperature)
+        {
+            return (1.8 * temperature) + 32;
+        }
+
+        public static void Main()
+        {
+            const string PrimerMissatge = "Introdueix la temperatura en Celsius (Cº): ";
+            const string MissatgeResultat = "La teva temperatura en Farenheit és";
+            const string MissatgeError = "Format incorrecte";
+
+            double numUser;
+
+            Console.Write(PrimerMissatge);
+            try
+            {
+                numUser = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine($"{MissatgeResultat} {CelsiusToFarenheit(numUser)}");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine(MissatgeError);
+            }
+        }
+    }
+}
 //CON:
 ```
 
